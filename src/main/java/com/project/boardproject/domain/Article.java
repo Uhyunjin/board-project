89984c0663d7e0cb019474241c6b6dce5a512ab2
유현진 <98440593+Uhyunjin@users.noreply.github.com>
2024-04-17 15:29:07 +0900

    #10-주석추가
    이해가 모자란 부분 검색하면서 주석 추가

--- src/main/java/com/project/boardproject/domain/Article.java
+++ src/main/java/com/project/boardproject/domain/Article.java
@@ -49,23 +49,30 @@ public class Article {
     @LastModifiedBy @Column(nullable = false, length = 100)
     private String modifiedBy; //수정자
 
-    protected Article() {}
+    protected Article() {} //private은 에러가 난다 hibernate문서 확인하기(my batis 대신 사용)
 
     private Article(String title, String content, String hashtag) {
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
-    }
+    } // 메타데이터->자동 아이디->set불가능 도메인과 관련된 정보만 오픈하고 생성
 
     public static Article of(String title, String content, String hashtag) {
         return new Article(title, content, hashtag);
-    }
+    } // of함수는 객체로서 최소조건을 만족한 채 만들어지고
+      // entity일 경우 db저장, dto일 경우 네트워크 전송이 바로 가능해야한다
 
+        
+    // 리스트에 넣거나, 중복요소 제거, 정렬 시 필요한 동등성 검사(equals & hashcode)
+    // 모두 다 검사하지 않고 id만 검사해도(unique) 충분
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Article article)) return false;
+            // pattern variable
+            
         return id!= null && id.equals(article.id);
+            // db에 영속화 전의 아티클은 id를 부여받지 않은 상태로, 다른것으로 간주
     }
 
     @Override
